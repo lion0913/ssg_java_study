a7233f83ed48ccb637f51fdebc92130b148d2bb4
dajung <ekwjd990913@gmail.com>
2022-07-07 14:31:34 +0900

    명령 프롬프트 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,7 +1,26 @@
 package com.ll.exam;
 
+import java.util.Scanner;
+
 public class App {
     public void run() {
         System.out.println("== 명언 SSG ==");
+
+        //키보드 입력
+        Scanner sc = new Scanner(System.in);
+
+        outer:while(true) {
+            System.out.print("명령) ");
+            //입력받음+공백제거
+            String cmd = sc.nextLine().trim();
+
+            switch(cmd)  {
+                //종료 입력시 outer(while문 자체를) 종료
+                case "종료":
+                    break outer;
+            }
+        }
+
+        sc.close();
     }
 }
